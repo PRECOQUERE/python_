cb260c6c25a3fd512801d7454a472371e130f0b4
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-05 23:42:25 +0900

    Add files via upload

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,22 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from menu import Menu, MenuItem         #주문 받는 기계, 메뉴판
+from coffee_maker import CoffeeMaker    #커피 만드는 기계
+from money_machine import MoneyMachine  #돈 받는 기계
+
+Waiter = Menu()
+Maxim = CoffeeMaker()
+Cashier = MoneyMachine()
+
+is_on = True
+
+while(is_on) :
+    print("What do u want to order?")
+    pick_drinks = input(Waiter.get_items() + ' ')
+    if Waiter.find_drink(pick_drinks) :
+        if Cashier.make_payment(Waiter.find_drink(pick_drinks).cost) :
+            if Maxim.is_resource_sufficient(pick_drinks) :
+                Maxim.make_coffee(pick_drinks)
+            else :
+                print("Try again later.")
+                break
+    else :
+        print("Choose another one plz.")
