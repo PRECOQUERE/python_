8d9aaa406afbd940bec51dffa0c95549fe07bcff
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-06 11:13:10 +0900

    Add files via upload

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,21 +2,32 @@ from menu import Menu, MenuItem         #주문 받는 기계, 메뉴판
 from coffee_maker import CoffeeMaker    #커피 만드는 기계
 from money_machine import MoneyMachine  #돈 받는 기계
 
-Waiter = Menu()
-Maxim = CoffeeMaker()
+# 2023/04/06
+# 'str' object has no attribute 'ingredients' 오류가 났었음.
+# 클래스 선언 순서에 문제가 있었던 걸로 보임.
+# 선언 순서에 따라 클래스 속성으로 받아들일 수도 받아들이지 못할 수도 있으니 확인해서 코딩하기
 Cashier = MoneyMachine()
+Maxim = CoffeeMaker()
+Waiter = Menu()
 
 is_on = True
 
 while(is_on) :
-    print("What do u want to order?")
-    pick_drinks = input(Waiter.get_items() + ' ')
-    if Waiter.find_drink(pick_drinks) :
-        if Cashier.make_payment(Waiter.find_drink(pick_drinks).cost) :
-            if Maxim.is_resource_sufficient(pick_drinks) :
-                Maxim.make_coffee(pick_drinks)
+    options = Waiter.get_items()
+    choice = input(f"What would you like ? ({options}) ")
+    if choice == "off" :
+        is_on = False        
+    elif choice == "report" :
+        Maxim.report()
+        Cashier.report()
+    else :
+        drink = Waiter.find_drink(choice)
+        if drink:
+            if Maxim.is_resource_sufficient(drink) and Cashier.make_payment(drink.cost) :
+                Maxim.make_coffee(drink)
             else :
                 print("Try again later.")
-                break
-    else :
-        print("Choose another one plz.")
+        else :
+            print("Choose another one plz.")
+
+
