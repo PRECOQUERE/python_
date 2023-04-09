95fd9933bce37ddfedcd358656921331e992546b
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-09 14:50:57 +0900

    Add files via upload

---
+++ day-18-start_hirstPainting/_main.py
@@ -0,0 +1,35 @@
+
+import turtle as t
+import random
+import colorgram
+
+tim = t.Turtle()
+t.colormode(255)
+tim.speed("fastest")
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_shape(sides) :
+    angle = 360 / sides
+    for _ in range(sides):
+        tim.fd(100)
+        tim.right(angle)       
+
+directions = [0, 90, 180, 270]
+def draw_random(dir):
+    tim.right(random.choice(directions))
+    tim.fd(30)
+
+for _ in range(1, 360, 5) :
+    tim.color(random_color())
+    tim.circle(100, None, None)
+    # tim.left(5)tim.setheading(tim.heading() + gap...)
+
+#그림판 띄우는 부분
+screen = t.Screen()
+screen.exitonclick()
--- day-18-start_hirstPainting/main.py
+++ day-18-start_hirstPainting/main.py
@@ -1,35 +1,35 @@
-
+#허슬러 페인터
+import colorgram as cg
 import turtle as t
-import random
+import random as rd
+# rgb_colors = []
+# colors = cg.extract('day-18-start_hirstPainting/image.jpg', 30)
+# for color in colors :
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)   
+color_list = [(237, 205, 82), (204, 143, 165), (196, 181, 96), (158, 72, 44), (66, 111, 131), (125, 39, 37), (143, 67, 110), (221, 230, 232), (236, 244, 241), (172, 159, 48), (105, 142, 172), (4, 131, 68), (148, 35, 65), (56, 34, 54), (136, 186, 176), (1, 117, 79), (43, 36, 57), (36, 35, 33), (57, 172, 175), (233, 172, 154), (63, 171, 169), (70, 68, 55), (211, 179, 189), (180, 95, 87), (155, 111, 127), (165, 206, 193), (123, 126, 135), (166, 204, 207)]
+t.colormode(255)
 
 tim = t.Turtle()
-t.colormode(255)
 tim.speed("fastest")
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
+tim.ht()
+tim.pu()
+pos_x = -300
+pos_y = -300
+for i in range(10) :
+    tim.setpos(pos_x, pos_y)
+    for j in range(10) :
+        tim.pd()
+        tim.dot(20, rd.choice(color_list))
+        tim.pu()
+        pos_x += 50
+        tim.setx(pos_x)
+    pos_x = -300
+    pos_y += 50
 
-def draw_shape(sides) :
-    angle = 360 / sides
-    for _ in range(sides):
-        tim.fd(100)
-        tim.right(angle)       
-
-directions = [0, 90, 180, 270]
-def draw_random(dir):
-    tim.right(random.choice(directions))
-    tim.fd(30)
-
-for _ in range(1, 360, 5) :
-    tim.color(random_color())
-    tim.circle(100, None, None)
-    tim.left(5)
-    #tim.setheading(tim.heading() + gap...)
-
-#그림판 띄우는 부분
 screen = t.Screen()
 screen.exitonclick()
