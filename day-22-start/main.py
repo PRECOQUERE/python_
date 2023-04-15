be11e43058cc1485610be1c7d993ac167f1318cd
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-15 21:12:14 +0900

    Delete day-22-start directory

--- day-22-start/main.py
+++
@@ -1,8 +0,0 @@
-# 1. create Screen
-# 2. create my Paddle & move my Paddle
-# 3. create another Paddle & move Paddle
-# 4. create Ball & move Ball
-#     - detect collision with wall and bounce
-#     - detect with paddle
-#     - detect when paddle misses
-# 5. create Scoreboard
