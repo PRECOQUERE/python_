91b6e27280058100bf7957c9c99ba0e0642ddf7a
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-19 23:24:16 +0900

    Add files via upload

---
+++ day-24-start/Mail Merge Project Start/main.py
@@ -0,0 +1,20 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+file_dear = open('day-24-start/Mail Merge Project Start/Input/Names/invited_names.txt')
+list_dear = file_dear.readlines()
+file_letter = open('day-24-start/Mail Merge Project Start/Input/Letters/starting_letter.txt')
+letter = file_letter.read()
+
+for dear in list_dear:
+    dear = dear.strip()
+    dear_letter = letter.replace('[name]', dear)
+    with open(f"day-24-start/Mail Merge Project Start/Output/{dear}.txt", mode='w') as file:
+        file.write(dear_letter)
+
