c400e883f4ac23f543c53b954dcef11e3ac59050
PRECOQUERE <106543544+PRECOQUERE@users.noreply.github.com>
2023-04-20 10:16:42 +0900

    Add files via upload

--- day-24-start/Mail Merge Project Start/main.py
+++ day-24-start/Mail Merge Project Start/main.py
@@ -7,16 +7,13 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-file_dear = open('day-24-start/Mail Merge Project Start/Input/Names/invited_names.txt')
-list_dear = file_dear.readlines()
-file_letter = open('day-24-start/Mail Merge Project Start/Input/Letters/starting_letter.txt')
-letter = file_letter.read()
+with open('day-24-start/Mail Merge Project Start/Input/Letters/starting_letter.txt', mode='r') as letter :
+    content_letter = letter.read()
+with open('day-24-start/Mail Merge Project Start/Input/Names/invited_names.txt', mode='r') as dear :
+    list_dear = dear.readlines()
 
 for dear in list_dear:
     dear = dear.strip()
-    dear_letter = letter.replace('[name]', dear)
-    with open(f"day-24-start/Mail Merge Project Start/Output/{dear}.txt", mode='w') as file:
+    dear_letter = content_letter.replace('[name]', dear)
+    with open(f'day-24-start/Mail Merge Project Start/Output/ReadyToSend/{dear}.txt', mode='w') as file:
         file.write(dear_letter)
-
-file_dear.close()
-file_letter.close()
